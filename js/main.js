d9f263793272bb041d598b2330a1dbb3ff2d8d08
jake <singviy@gmail.com>
2017-05-09 08:40:35 +0800

    add

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by jake on 2017/5/4.
+ */
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
